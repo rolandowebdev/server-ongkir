9bd3aba8c6a20f9ad105f5911b804d12c29d9abc
RolandoPranataAR <rolandopranata@gmail.com>
2022-08-10 01:27:48 +0700

    initial commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+const dataRouter = require('./routes'); // routes for rajaongkir api
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use('/', dataRouter); // setup base path & get data api from routes
+
+module.exports.handler = serverless(app);
---
+++ src/routes.js
@@ -0,0 +1,38 @@
+const { init } = require('rajaongkir-node-js');
+
+const express = require('express');
+const router = express.Router();
+
+const request = init('1318ee5c9696bf6e44f28baba2846ea0', 'starter');
+
+router.get('/provinsi', (req, res) => {
+  const province = request.get('/province');
+  province.then((prov) => {
+    const js = JSON.parse(prov);
+    res.send(js);
+  });
+});
+
+router.get('/kota/:id', (req, res) => {
+  const allCityInProvince = request.get(`/city?&province=${req.params.id}`);
+  allCityInProvince.then((city) => {
+    const citi = JSON.parse(city);
+    res.send(citi);
+  });
+});
+
+router.post('/ongkir', (req, res) => {
+  const form = req.body;
+  const data = {
+    origin: form.origin,
+    destination: form.destination,
+    weight: form.weight,
+    courier: form.courier, // can request other courier
+  };
+  const cost = request.post('cost', data);
+  cost.then((cst) => {
+    res.send(cst);
+  });
+});
+
+module.exports = router;
